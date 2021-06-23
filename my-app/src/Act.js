c782c495db5c05118885867f108885002dd9bf53
apgearhart1 <apgearhart1@gmail.com>
2021-06-22 22:39:47 -0500

    Add Navbar

---
+++ my-app/src/Act.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Act extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,56 @@
 import React from 'react';
 import './App.css';
-import List from './MovieList'
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+import { Route, Switch, withRouter } from "react-router-dom";
+import Act from './Act';
+import Movies from './Movies';
+import TV from './Tv'
 function App() {
   return (
     <div className="App" >
+
+      {/* <div className="nav"> */}
+        <Navbar bg="dark" variant="dark">
+          <Nav className="container-fluid">
+            <Nav.Link className="ml-auto">Movies</Nav.Link>
+            <Nav.Link className="ml-auto">TV Shows</Nav.Link>
+            <Nav.Link>Actors/Actresses</Nav.Link>
+            
+            <Form inline>
+              <FormControl
+                type="text" placeholder="Search" className="mr-sm-2"
+                />
+              <Button variant="outline-info">
+                Search
+              </Button>
+            </Form>
+          </Nav>
+          
+        </Navbar>
+
+
+        <Switch>
+
+
+          <Route exact path="/" component={App} />
+          <Route exact path="/actors" component={Act} />
+          <Route exact path="/movies" component={Movies} />
+          <Route exact path="/tv" component={TV} />
+
+
+        </Switch>
+
+
+      {/* </div> */}
       <header className="App-header">
+
+
+
         <h1>ReelTime</h1>
         <h2>A cleaner movie DB experience</h2>
       </header>
-
-      <div>
-      <h2>Popular Movies</h2>
-        <List/>
-      </div> 
     </div>
   );
 }
 
-export default App;
+export default withRouter(App);
